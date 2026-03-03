9fdb4c414e76a25cecb66b4bf1c640e24ec2482b
Giovanni Gianfriddo <giovannigianfriddo004@gmail.com>
2026-03-03 16:29:46 +0100

    Notes for Programming and Laboratory class on C++

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){
+    std::cout<<"Hello World\n"<<std::endl;
+    return 0;
+}
+
+/* g++ test.cpp --std=c++14
+ Oggetto --> Area di memoria a cui è assegnato un tipo
+ Variabile --> Oggetto a cui è attribuito un identificatore
+ C++ è un linguaggio "strongly typed"
+*/
